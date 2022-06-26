1fad68113419df7b4c36c0405e23b0f782dbd89b
Yuki Ogawa <yuki@Yukis-MacBook-Pro.local>
2022-06-27 05:47:25 +0900

    Handle GET /auth request

---
+++ server/routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const auth = require('../database/controllers/auth');
+
+router.get('/auth', (req, res) => {
+  const { username, password } = req.query;
+
+  auth.find(username, password)
+    .then((data) => res.status(200).send(data))
+    .catch((err) => res.status(500).send('Internal Server Error', err));
+});
+
+module.exports = router;
