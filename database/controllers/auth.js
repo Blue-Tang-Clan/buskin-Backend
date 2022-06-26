24f4ec59a7bef9bd91fabd5e6fc5a88f52c716bb
Yuki Ogawa <yuki@Yukis-MacBook-Pro.local>
2022-06-27 05:15:25 +0900

    Create auth table controller and add basic query function

---
+++ database/controllers/auth.js
@@ -0,0 +1,14 @@
+const client = require('../index');
+
+const find = function (username, password) {
+  return client.query(`
+    SELECT *
+    FROM auth
+    WHERE username = '${username}'
+    AND password = '${password}'
+    `)
+    .then((data) => data.rows)
+    .catch((err) => console.error('Query Error', err));
+};
+
+module.exports = { find };
