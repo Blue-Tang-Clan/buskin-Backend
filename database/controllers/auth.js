348df646331c1e73ff7b1fff56b2712dabf0820e
Yuki Ogawa <yuki@Yukis-MacBook-Pro.local>
2022-06-27 07:39:40 +0900

    Fix syntax bugs and create add function that inserts values into db

--- database/controllers/auth.js
+++ database/controllers/auth.js
@@ -4,11 +4,19 @@ const find = function (username, password) {
   return client.query(`
     SELECT *
     FROM auth
-    WHERE username = ${username}
-    AND password = ${password}
-    `)
+    WHERE username = '${username}'
+    AND password = '${password}'
+  `)
     .then((data) => data.rows)
     .catch((err) => console.error('Query Error', err));
 };
 
-module.exports = { find };
+const add = function (username, email, password, type) {
+  return client.query(`
+    INSERT INTO auth (username, email, password, type)
+    VALUES ('${username}', '${email}', '${password}', '${type}');
+  `)
+    .catch((err) => console.error('Insert Error', err));
+};
+
+module.exports = { find, add };
