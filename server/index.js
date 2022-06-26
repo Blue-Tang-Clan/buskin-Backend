5c1f7b1b974acb3e2a8abb846ecb067b36059e6c
Yuki Ogawa <yuki@Yukis-MacBook-Pro.local>
2022-06-27 05:18:19 +0900

    Test the auth controller

--- server/index.js
+++ server/index.js
@@ -10,3 +10,10 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`successful connected to ${port}`);
 });
+
+/* Testing connection to database and querying  */
+const auth = require('../database/controllers/auth');
+
+auth.find('yuki', '123thumbwar')
+  .then((data) => console.log('Query Result', data))
+  .catch((err) => console.error('Query Error', err));
