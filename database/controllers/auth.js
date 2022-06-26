e771dc9fa24933c48d64829d106244825a0cc7f2
Yuki Ogawa <yuki@Yukis-MacBook-Pro.local>
2022-06-27 05:46:16 +0900

    Fix sytax bugs

--- database/controllers/auth.js
+++ database/controllers/auth.js
@@ -4,8 +4,8 @@ const find = function (username, password) {
   return client.query(`
     SELECT *
     FROM auth
-    WHERE username = '${username}'
-    AND password = '${password}'
+    WHERE username = ${username}
+    AND password = ${password}
     `)
     .then((data) => data.rows)
     .catch((err) => console.error('Query Error', err));
