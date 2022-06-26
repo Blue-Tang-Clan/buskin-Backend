919ed6f8965d73d41f8ba22e10d0ea1370370122
Yuki Ogawa <yuki@Yukis-MacBook-Pro.local>
2022-06-27 05:46:44 +0900

    Remove controller tests and add route handler

--- server/index.js
+++ server/index.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const router = require('./routes');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+// All requests are handled in the routes.js file
+app.use('/', router);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`successful connected to ${port}`);
 });
-
-/* Testing connection to database and querying  */
-const auth = require('../database/controllers/auth');
-
-auth.find('yuki', '123thumbwar')
-  .then((data) => console.log('Query Result', data))
-  .catch((err) => console.error('Query Error', err));
