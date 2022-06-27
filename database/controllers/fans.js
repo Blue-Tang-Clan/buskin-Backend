2d8066ffcba4c5e5ce10caa6d8a4a3db97394802
nelliott82 <nikkoelliott@gmail.com>
2022-06-27 19:54:20 -0400

    Updated controllers to not be callback style

--- database/controllers/fans.js
+++ database/controllers/fans.js
@@ -2,70 +2,21 @@ var models = require('../models');
 
 module.exports = {
   get: function(req, res) {
-    models.fans.get(req.params.fanId, function(err, result) {
-      if (err) {
-        res.status(500);
-        console.log('GET Fan Info error: ', err);
-        res.end(JSON.stringify(err));
-      } else {
-        res.statusCode = 200;
-        res.end(JSON.stringify(result));
-      }
-    })
+    models.fans.get(req, res)
   },
   saveEvent: function(req, res) {
-    models.fans.saveEvent(req.body, function(err, result) {
-      if (err) {
-        res.status(500);
-        console.log('Save Event Fan error: ', err);
-        res.end(JSON.stringify(err));
-      } else {
-        res.sendStatus(201);
-      }
-    })
+    models.fans.saveEvent(req, res);
   },
   followArtist: function(req, res) {
-    models.fans.followArtist(req.body, function(err, result) {
-      if (err) {
-        res.status(500);
-        console.log('Follow Artist Fan error: ', err);
-        res.end(JSON.stringfiy(err));
-      } else {
-        res.sendStatus(201);
-      }
-    })
+    models.fans.followArtist(req, res);
   },
   put: function(req, res) {
-    models.fans.put(req.body, function(err, result) {
-      if (err) {
-        res.status(500);
-        console.log('Update profile Fan error: ', err);
-        res.end(JSON.stringify(err));
-      } else {
-        res.sendStatus(204);
-      }
-    })
+    models.fans.put(req, res);
   },
   removeEvent: function(req, res) {
-    models.fans.removeEvent(req.params.eventId, req.params.fanId, function(err, result) {
-      if (err) {
-        res.status(500);
-        console.log('Remove event Fan error: ', err);
-        res.end(JSON.stringify(err));
-      } else {
-        res.sendStatus(201);
-      }
-    })
+    models.fans.removeEvent(req, res);
   },
   unfollowArtist: function(req, res) {
-    models.fans.removeEvent(req.params.eventId, req.params.fanId, function(err, result) {
-      if (err) {
-        res.status(500);
-        console.log('Unfollow artist Fan error: ', err);
-        res.end(JSON.stringify(err));
-      } else {
-        res.sendStatus(201);
-      }
-    })
+    models.fans.removeEvent(req, res);
   }
 }
