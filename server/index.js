7ff20214dde149d20a714b2eb5772a4fc02825fa
nelliott82 <nikkoelliott@gmail.com>
2022-06-27 11:40:07 -0400

    Staged changes for router

--- server/index.js
+++ server/index.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const router = require('./routes');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-// All requests are handled in the routes.js file
-app.use('/', router);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
