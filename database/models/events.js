a0616e8ff5796957c91bdfb284a052b9568880e5
nelliott82 <nikkoelliott@gmail.com>
2022-06-27 21:21:47 -0400

    Added models for events for get all and get one. Currently no by date functionality for get all.

---
+++ database/models/events.js
@@ -0,0 +1,36 @@
+const client = require('../index');
+
+module.exports = {
+  getAll: function(req, res) {
+    client.query(`
+      SELECT *
+      FROM events
+    `)
+    .then((data) => {
+      res.statusCode = 200;
+      res.end(JSON.stringify(data));
+    })
+    .catch((err) => {
+      res.status(500);
+      console.log('GET Events error: ', err);
+      res.end(JSON.stringify(err));
+    });
+  },
+  saveEvent: function(req, res) {
+    client.query(`
+      SELECT *
+      FROM events e
+      WHERE e.id = '${req.params.eventId}'
+    `)
+    .then((data) => {
+      res.statusCode = 200;
+      res.end(JSON.stringify(data));
+    })
+    .catch((err) => {
+      res.status(500);
+      console.log(`GET Event ID ${req.params.eventId} error: `, err);
+      res.end(JSON.stringify(err));
+    });
+  }
+  }
+}
