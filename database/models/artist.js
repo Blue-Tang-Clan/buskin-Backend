0525989a334fe8ace0e38421c527840148d22a8f
nelliott82 <nikkoelliott@gmail.com>
2022-06-29 14:44:13 -0400

    Cleaned up query for readability and added followers array with emails for notifications

--- database/models/artist.js
+++ database/models/artist.js
@@ -4,15 +4,51 @@ const artist = {
   get: (req, res) => {
     console.log('we get this far', req);
     client.query(`
-      select json_build_object('id', a.id, 'name', a.display_name, 'bio', a.bio,
-        'genre', a.genre, 'instrument', a.instrument, 'pic', a.pic, 'venmo', a.venmo, 'cashapp', a.cashapp, 'paypal', a.paypal,
+      select json_build_object(
+        'id', a.id,
+        'name', a.display_name,
+        'bio', a.bio,
+        'genre', a.genre,
+        'instrument', a.instrument,
+        'pic', a.pic,
+        'venmo', a.venmo,
+        'cashapp', a.cashapp,
+        'paypal', a.paypal,
 
-        'events', (select array_to_json(array_agg(json_build_object('id', e.id, 'name', e.name, 'street', e.street,
-        'city', e.city, 'state', e.state, 'latitude', e.latitude, 'longitude', e.longitude, 'date', e.date,
-        'start_time', e.start_time, 'end_time', e.end_time)))
-        from event as e
-        where e.art_id = ${req.params.artist_id}))
-
+        'events', (select
+          array_to_json(
+            array_agg(
+              json_build_object(
+                'id', e.id,
+                'name', e.name,
+                'street', e.street,
+                'city', e.city,
+                'state', e.state,
+                'latitude', e.latitude,
+                'longitude', e.longitude,
+                'date', e.date,
+                'start_time', e.start_time,
+                'end_time', e.end_time
+              )
+            )
+          )
+          from event as e
+          where e.art_id = ${req.params.artist_id}
+        ),
+        'followers', (select
+          array_to_json(
+            array_agg(
+              json_build_object(
+                'email', (SELECT auth.email FROM auth WHERE auth.id = f.auth_id)
+              )
+            )
+          )
+          FROM fans f, art_fan af
+          WHERE af.fan_id = f.id
+          AND af.art_id = ${req.params.artist_id}
+          GROUP BY af.art_id
+        )
+      )
       from artists a
       where a.id = ${req.params.artist_id};
     `)
