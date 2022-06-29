6094bec0591aa4e87cb1075f75a33a7db1c9f426
YK Dong <ykdong1991@gmail.com>
2022-06-28 20:20:56 -0400

    modify query result for search event table

--- database/controllers/homepage.js
+++ database/controllers/homepage.js
@@ -1,11 +1,6 @@
-const model = require('../models');
+const homePage = require('../models/homePage');
 
 module.exports = {
-
-  getHomePage: (req, res) => {
-    // get 5 upcoming events
-    // get 5 most popular artists
-    // get 1 newest artist as fresh talent
-  },
-
+  getHomePageInfo: (req, res) => { homePage.getHomePageInfo(req, res); },
+  getHomePageGenre: (req, res) => { homePage.getHomePageGenre(req, res); },
 };
--- database/models/homePage.js
+++ database/models/homePage.js
@@ -1,7 +1,35 @@
 const client = require('../index');
 
 const homePage = {
-
+  getHomePageInfo: (req, res) => {
+    const { longitude, latitude } = req.query;
+    const range = 0.1;
+    client.query(` 
+      SELECT * FROM public.event
+      WHERE date::date >= CURRENT_DATE
+      AND longitude <= ${} + ${range} AND longitude >= ${} - ${range}
+      AND latitude <= ${} + ${range} AND latitude >= ${} - ${range}
+      ORDER BY date, start_time DESC
+      LIMIT 5; 
+    `)
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  },
+  getHomePageGenre: (req, res) => {},
 };
 
-module.exports =  homePage;
+module.exports = homePage;
+
+// result = {
+//   // get 5 upcoming events
+//   events: [
+//     {},
+//   ],
+//   // get 5 most popular artists
+//   artists: [
+//     {},
+//   ],
+//   // get 1 newest artist as fresh talent
+//   talent: {} // artist id 20
+
+// }
