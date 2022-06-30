c909acbaa3a0681f8af8133932ccd55f5f0e29c0
nelliott82 <nikkoelliott@gmail.com>
2022-06-30 15:43:19 -0400

    Changed code to only run toLowerCase on query once.

--- database/models/search.js
+++ database/models/search.js
@@ -2,7 +2,8 @@ const client = require('../index');
 
 const search = {
   get: (req, res) => {
-    const { query } = req.query;
+    let { query } = req.query;
+    query = query.toLowerCase();
     client.query(`
       SELECT json_build_object(
         'artists', (
@@ -11,9 +12,9 @@ const search = {
           'name', a.display_name,
           'pic', a.pic )))
           FROM artists a
-          WHERE lower(a.display_name) LIKE '%${query.toLowerCase()}%'
-          OR lower(a.instrument) LIKE '%${query.toLowerCase()}%'
-          OR lower(a.genre) LIKE '%${query.toLowerCase()}%'
+          WHERE lower(a.display_name) LIKE '%${query}%'
+          OR lower(a.instrument) LIKE '%${query}%'
+          OR lower(a.genre) LIKE '%${query}%'
         ),
         'event', (
           SELECT array_to_json(array_agg(json_build_object(
@@ -23,10 +24,10 @@ const search = {
           'state', e.state,
           'pic', e.pic )))
           FROM event e
-          WHERE lower(e.name) LIKE '%${query.toLowerCase()}%'
-          OR lower(e.city) LIKE '%${query.toLowerCase()}%'
-          OR lower(e.street) LIKE '%${query.toLowerCase()}%'
-          OR lower(e.state) LIKE '%${query.toLowerCase()}%'
+          WHERE lower(e.name) LIKE '%${query}%'
+          OR lower(e.city) LIKE '%${query}%'
+          OR lower(e.street) LIKE '%${query}%'
+          OR lower(e.state) LIKE '%${query}%'
         )
         )`)
       .then((data) => { res.status(200).json(data.rows[0]); })
